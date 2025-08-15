021a11684beab21e00042e60154be79f0b832ca1
Aakarsh Verma <ansh62050@gmail.com>
2025-08-16 05:00:04 +0530

    Initilised project

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+export default app;
