b969eee0a9a4399e930860d89f81be687c0ed6db
Aakarsh Verma <ansh62050@gmail.com>
2025-09-03 06:59:32 +0530

    updated user router

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { isAuthenticatedUser } from "../middlewares/authenticateUser.js";
-import { matches, pendingRequests } from "../controllers/user.controller.js";
+import {
+  matches,
+  pendingRequests,
+  feed,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/requests/pending", isAuthenticatedUser, pendingRequests);
 router.get("/matches", isAuthenticatedUser, matches);
+router.get("/feed", isAuthenticatedUser, feed);
 export default router;
